3dbc5e45033618e06e24a677051ffc552965ef6b
Ardie Castro <rd3cstr0@gmail.com>
2026-05-14 16:38:30 +0400

    Use environment variable for app secret

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello from DevOps CI/CD Pipeline v2! --RD"
+    app_secret = os.gatenv("APP_SECRET", "No Secret Found")
 
+    return f"Hello from DevOps CI/CD Pipeline v2! Secret: {app_secret}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+   app.run(host="0.0.0.0", port=5000)
