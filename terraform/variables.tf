0401795aa99c72115773b10f3c19016013488382
Ardie Castro <rd3cstr0@gmail.com>
2026-07-08 11:34:14 +0400

    infra: fix variable for resource group

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "location" {
 variable "resource_group_name" {
   description = "Resource group name"
   type        = string
-  default     = "DEVOPS-TERRAFORM-RG"
+  default     = "devops-terraform-rg"
 }
 
 variable "admin_username" {
